8877a4178d3d1f261fd265c09da7eb54803fa6d7
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-08-14 20:03:45 +0100

    task 1 done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -20,3 +20,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable[[bytes], Union[str, int]]] = None
+            ) -> Optional[Union[str, int]]:
+        """ return the value of given key with spicific
+        format or just like it"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is None:
+            return value
+        return fn(value)
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ return the value of a key in str format"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """ return the value of a key in int format"""
+        return self.get(key, lambda x: int(x))
